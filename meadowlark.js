3fbdd777d3bfafd382c5e8620316ca504522bea4
jsbalrog <edwardtjenkins@gmail.com>
2014-08-10 08:33:46 -0600

    Chapter 9 commit

--- meadowlark.js
+++ meadowlark.js
@@ -3,6 +3,10 @@ var fortune = require('./lib/fortune.js');
 var formidable = require('formidable');
 var app = express();
 var jqupload = require('jquery-file-upload-middleware');
+var credentials = require('./credentials.js');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var expressSession = require('express-session');
 
 // set up handlebars view engine
 var handlebars = require('express3-handlebars').create({ defaultLayout:'main' });
@@ -26,7 +30,22 @@ app.use(function(req, res, next) {
 	next();
 });
 
-app.use(require('body-parser')());
+// to handle POST data
+app.use(bodyParser());
+
+// to handle cookies
+app.use(cookieParser(credentials.cookieSecret));
+// to store session info server-side via cookies
+app.use(expressSession());
+
+// remove any existing flash messages in the session
+app.use(function(req, res, next) {
+	// if there's a flash message, transfer
+	// it to the context, then clear it
+	res.locals.flash = req.session.flash;
+	delete req.session.flash;
+	next();
+});
 
 // routes:
 app.get('/', function(req, res) {
@@ -41,16 +60,36 @@ app.get('/newsletter', function(req, res) {
 	res.render('newsletter', { csrf: 'CSRF token goes here' });
 });
 
-app.post('/process', function(req, res) {
-	if(req.xhr || req.accepts('json,html') === 'json') {
-		// if there were an error, we would send { error: 'description' }
-		res.send({ success: true });
-	} else {
-		console.log('Form (from querystring):', req.query.form);
-		console.log('CSRF token (from hidden form field):', req.body._csrf);
-		console.log('Email (from visible form field):', req.body.email);
-		res.redirect(303, '/thank-you');
+app.post('/newsletter', function(req, res) {
+	var name = req.body.name || '', email = req.body.email || '';
+	// input validation
+	if(!email.match(VALID_EMAIL_REGEX)) {
+		if(req.xhr) return res.json({ error: 'Invalid name email address.' });
+		req.ression.flash = {
+			type: 'danger',
+			intro: 'Validation error!',
+			message: 'The email address you entered was not valid.',
+		};
+		return res.redirect(303, '/newsletter/archive');
 	}
+	new NewsletterSignup({ name: name, email: email }).save(function(err) {
+		if(err) {
+			if(req.xhr) return res.json({ error: 'Database error.' });
+			req.session.flash = {
+				type: 'danger',
+				intro: 'Database error!',
+				message: 'There was a database error; please try again later.'
+			}
+			res.redirect(303, '/newsletter/archive');
+		}
+		if(req.xhr) return res.json({ success: true });
+		req.session.flash = {
+			type: 'success',
+			intro: 'Thank you!',
+			message: 'You have now been signed up for the newsletter.'
+		};
+		return res.redirect(303, '/newsletter/archive');
+	});
 });
 
 app.get('/contest/vacation-photo', function(req, res) {
@@ -71,7 +110,6 @@ app.post('/contest/vacation-photo/:year/:month', function(req, res) {
 });
 
 // jquery file upload
-
 app.get('/contest/funny-photo', function(req, res) {
   res.render('contest/funny-photo');
 });
