ecfa66c14712e0c4c6ef45c6518f283e11876fe4
jsbalrog <edwardtjenkins@gmail.com>
2014-08-10 08:32:35 -0600

    Chapter 8 commit

--- meadowlark.js
+++ meadowlark.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var fortune = require('./lib/fortune.js');
+var formidable = require('formidable');
 var app = express();
+var jqupload = require('jquery-file-upload-middleware');
 
 // set up handlebars view engine
 var handlebars = require('express3-handlebars').create({ defaultLayout:'main' });
@@ -24,6 +26,8 @@ app.use(function(req, res, next) {
 	next();
 });
 
+app.use(require('body-parser')());
+
 // routes:
 app.get('/', function(req, res) {
   res.render('home');
@@ -31,6 +35,57 @@ app.get('/', function(req, res) {
 
 app.get('/about', function(req, res) {
   res.render('about', { fortune: fortune() , pageTestScript: '/qa/tests-about.js' });
+});
+
+app.get('/newsletter', function(req, res) {
+	res.render('newsletter', { csrf: 'CSRF token goes here' });
+});
+
+app.post('/process', function(req, res) {
+	if(req.xhr || req.accepts('json,html') === 'json') {
+		// if there were an error, we would send { error: 'description' }
+		res.send({ success: true });
+	} else {
+		console.log('Form (from querystring):', req.query.form);
+		console.log('CSRF token (from hidden form field):', req.body._csrf);
+		console.log('Email (from visible form field):', req.body.email);
+		res.redirect(303, '/thank-you');
+	}
+});
+
+app.get('/contest/vacation-photo', function(req, res) {
+	var now = new Date();
+	res.render('contest/vacation-photo', { year: now.getFullYear(), month: now.getMonth() });
+});
+
+app.post('/contest/vacation-photo/:year/:month', function(req, res) {
+	var form = new formidable.IncomingForm();
+	form.parse(req, function(err, fields, files) {
+		if(err) return res.redirect(303, '/error');
+		console.log('received fields:');
+		console.log(fields);
+		console.log('received files:');
+		console.log(files);
+		res.redirect(303, '/thank-you');
+	});
+});
+
+// jquery file upload
+
+app.get('/contest/funny-photo', function(req, res) {
+  res.render('contest/funny-photo');
+});
+
+app.use('/upload', function(req, res, next) {
+  var now = Date.now();
+  jqupload.fileHandler({
+    uploadDir: function() {
+      return __dirname + '/public/uploads/' + now;
+    },
+    uploadUrl: function() {
+      return '/uploads/' + now;
+    }
+  })(req, res, next);
 });
 
 // custom 404 page
@@ -76,5 +131,5 @@ function getWeatherData() {
 				temp: '55.0 F (12.8 C)'
 			}
 		]
-	}
+	};
 }
